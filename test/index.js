ad512d0211691094ed05a948d6ba946476f5a48d
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-12-08 19:08:20 +0200

    Add tests

--- test/index.js
+++ test/index.js
@@ -1,14 +1,31 @@
 const { SqsParallel } = require('../src');
+const assert = require('assert');
+
+const RAND = new Date().getSeconds();
 
 const queue = new SqsParallel({
   region: 'eu-west-1',
-  name: 'sallar',
-  concurrency: 4,
+  name: 'test-queue',
+  concurrency: 1,
   debug: true
 });
+queue.on('error', err => {
+  console.error(err);
+  process.exit(1);
+});
 queue.on('message', message => {
-  message.deleteMessage().then(() => {
-    console.log('deleted');
-    message.next();
+  assert.equal(typeof message.data, 'object', 'An object should be received');
+  assert.equal(message.data.time, RAND, 'Received data must be same as sent');
+  message.deleteMessage().then(msg => {
+    assert.ok(msg, 'Message should be deleted');
   });
 });
+queue
+  .sendMessage({
+    body: {
+      time: RAND
+    }
+  })
+  .then(res => {
+    assert.ok(res, 'Message should be sent');
+  });
